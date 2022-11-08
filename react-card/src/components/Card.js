b59645fe7ddd564ef28a97b233e4ed5e2492bf95
Harry <odameharrison13@gmail.com>
2022-11-08 22:03:25 +0000

    first commit

---
+++ react-card/src/components/Card.js
@@ -0,0 +1,64 @@
+import React, { Component } from "react";
+
+class Card extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      email: "",
+      password: "",
+    };
+  }
+  handleChange = (e) => {
+    e.preventDefault();
+    this.setState({ [e.target.name]: e.target.value });
+  };
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state);
+  };
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <label>Name</label>
+            <br />
+            <input
+              type="text"
+              name="name"
+              value={this.state.name}
+              onChange={this.handleChange}
+            ></input>
+          </div>
+          <div>
+            <label>Email</label>
+            <br />
+            <input
+              type="email"
+              name="email"
+              value={this.state.email}
+              onChange={this.handleChange}
+            ></input>
+          </div>
+          <div>
+            <label>Password</label>
+            <br />
+            <input
+              type="password"
+              name="password"
+              value={this.state.password}
+              onChange={this.handleChange}
+            ></input>
+          </div>
+          <br />
+          <div>
+            <button>Submit</button>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Card;
